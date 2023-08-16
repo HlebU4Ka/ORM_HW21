d029b2a002347b48c079c9dc448d699c106ec289
glebk <glebkot57@gmail.com>
2023-08-16 20:14:59 +0200

    Initial commit

---
+++ main/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+
+# Create your models here.
+NULLABLE = {'null': True, 'blank': True}
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=150, name='name')
+    description = models.TextField(verbose_name='описание')
+    preview = models.ImageField(upload_to='products/', verbose_name='Изображение', **NULLABLE)
+    category = models.CharField(max_length=50, verbose_name='категория')
+    price = models.CharField(verbose_name='цена', **NULLABLE)
+    create_date = models.DateField(verbose_name='дата создания')
+    date_update = models.DateField(verbose_name='дата последнего изменения')
+
+    def __str__(self):
+        return (f'{self.name} - {self.category} - {self.price} - {self.create_date}' -
+                f' - {self.date_update}')
+
+    class Meta:
+        verbose_name = 'продукт'
+        verbose_name_plural = 'продукты'
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=150, name='name')
+    description = models.TextField(max_length=300, verbose_name='описание')
+
+
+    def __str__(self):
+        return f'{self.name} - {self.description}'
+
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'категории'

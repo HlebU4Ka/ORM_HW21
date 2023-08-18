5c67ca76f1014eb98585dffa317f69b9e91bf5a0
glebk <glebkot57@gmail.com>
2023-08-18 09:11:55 +0200

    create admin class

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
+from main.models import Product, Category
+
+
 # Register your models here.
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'description', 'price')
+    list_filter = ('category',)
+    search_fields = ('name', 'description')
+
+    def category_search(self, obj):
+        return obj.category.name if obj.category else '-'
+
+    category_search.short_description = 'Категория'
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'category')

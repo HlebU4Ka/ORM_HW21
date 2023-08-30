c813d65fcb4d34ad25a279ca3ee8ae0ec1ced9fe
glebk <glebkot57@gmail.com>
2023-08-30 21:38:47 +0200

    create admin class

--- main/views.py
+++ main/views.py
@@ -4,10 +4,9 @@ from .models import Product
 
 # Create your views here.
 
-def product_detail(request, product_id):
-    product = get_object_or_404(Product, product_id=product_id)
+def product_detail(request):
+    product = get_object_or_404(Product, id=id)
     return render(request, 'main/product_detail.html', {'product': product})
-
 
 def home(request):
     products = Product.objects.all()

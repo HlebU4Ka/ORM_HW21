cdc2a5624a01a943d2bcb7d962c895c0148e2232
glebk <glebkot57@gmail.com>
2023-09-03 15:18:54 +0200

    add blog

--- ORM_HW/settings.py
+++ ORM_HW/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'main',
-    # 'blog'
+    'blog'
 
 ]
 
---
+++ blog/urls.py
@@ -0,0 +1,14 @@
+# from django.contrib import admin
+# from django.urls import path
+#
+# from django.conf import settings
+# from main import views
+# from django.conf.urls.static import static
+#
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#     path('product/<int:product_id>/', views.product_detail, name='product_detail'),
+#     path('', views.home, name='home_page'),
+# ]
+# if settings.DEBUG:
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

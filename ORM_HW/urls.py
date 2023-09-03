ea741d14564e29a44f33200d8bbfa66cc1e05eef
glebk <glebkot57@gmail.com>
2023-09-03 13:14:43 +0200

    fix

--- ORM_HW/urls.py
+++ ORM_HW/urls.py
@@ -21,11 +21,10 @@ from django.conf import settings
 from main import views
 from django.conf.urls.static import static
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('product/<int:id>/', views.ProductDetailView.as_view, name='product_detail'),
-    path('', views.ProductListView.as_view(), name='home_page'),
+    path('product/<int:product_id>/', views.product_detail, name='product_detail'),  # Используйте views.product_detail
+    path('', views.home, name='home_page'),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

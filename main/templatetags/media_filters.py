7e33af04afbde6d8f18d9d8ced9fe2f40c4d3bec
glebk <glebkot57@gmail.com>
2023-09-03 10:51:15 +0200

    create admin class

--- main/templatetags/media_filters.py
+++ main/templatetags/media_filters.py
@@ -1,9 +1,11 @@
 from django import template
-from django.conf import settings
+
 
 register = template.Library()
 
 
-@register.filter(name='mediapath')
-def mediapath(value):
-    return f"{settings.MEDIA_URL}{value}"
+@register.filter
+def truncate_description(value, max_length=100):
+    if len(value) > max_length:
+        return value[:max_length] + "..."
+    return value
--- main/views.py
+++ main/views.py
@@ -8,6 +8,7 @@ def product_detail(request):
     product = get_object_or_404(Product, id=id)
     return render(request, 'main/product_detail.html', {'product': product})
 
+
 def home(request):
     products = Product.objects.all()
     return render(request, 'main/home.html', {'products': products})

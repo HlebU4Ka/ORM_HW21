22e23f3e922e555bc392049597559e062ad4c7f9
glebk <glebkot57@gmail.com>
2023-09-05 21:13:05 +0200

    создал html блог

--- main/urls.py
+++ main/urls.py
@@ -6,10 +6,7 @@ from main import views
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('main/', include('main.urls')),
     path('product/<int:product_id>/', views.product_detail, name='product_detail'),  # Используйте views.product_detail
-    path('', views.home, name='home_page'),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
